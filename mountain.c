9d08d9b131f22b2364740f4d4c4ceab8b3e3f7a3
larchey <charley.hoffm@gmail.com>
2024-02-12 09:44:37 -0600

    Move the noise

--- mountain.c
+++ mountain.c
@@ -7,6 +7,8 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 600
 
+int offsetX = 0, offsetY = 0; // Global variables to track the offset
+int offsetScale = 10;
 void drawCube(float x, float y, float z) {
     glBegin(GL_QUADS);
 
@@ -62,7 +64,7 @@ void drawGrid() {
 
     for (int i = 0; i < 20; ++i) {
         for (int j = 0; j < 20; ++j) {
-            float noiseValue = perlin(i * noiseScale, j * noiseScale, 0.0);
+            float noiseValue = perlin((i + offsetX) * noiseScale, (j + offsetY) * noiseScale, 0.0);
             int height = (int)floor(noiseValue * maxHeight);
             for (int k = 0; k <= height; ++k) {
                 drawCube(i - 10, k, j - 10);
@@ -77,7 +79,7 @@ void setupIsometricView(int width, int height) {
     gluPerspective(30.0, (double)width / (double)height, 0.1, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(20, 20, 20, 0, 0, 0, 0, 1, 0);
+    gluLookAt(20, 30, 30, 0, 0, 0, 0, 1, 0);
     glEnable(GL_POLYGON_SMOOTH);
     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
 }
@@ -96,8 +98,17 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         SDL_Event event;
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
-            break;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                break;
+            } else if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_w: offsetY -= offsetScale; break;
+                    case SDLK_s: offsetY += offsetScale; break;
+                    case SDLK_a: offsetX -= offsetScale; break;
+                    case SDLK_d: offsetX += offsetScale; break;
+                }
+            }
         }
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
